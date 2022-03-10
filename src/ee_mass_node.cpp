24358138a05efc33b5dd396e896fd66c31a49094
jaeu-choe <jaeuchoe@snu.ac.kr>
2022-03-10 19:57:39 +0900

    constant added

--- src/ee_mass_node.cpp
+++ src/ee_mass_node.cpp
@@ -9,13 +9,30 @@
 using Eigen::Matrix3d;
 using Eigen::Matrix;
 using Eigen::Vector3d;
+using Eigen::Quaterniond;
+
+// constant declaration
+double const L1 = 1.04;
+double const L2 = 1.01;
+double const L3 = 1.01;
+double const m1 = 1.9012;
+double const m2 = 1.9749;
+double const m3 = 1.4950;
+double const g_const = 9.807;
+Vector3d g = Vector3d(0, 0, -g_const);
+
+void link1_pose_callback(geometry_msgs::PoseStamped msg)
+{
+    //p_vicon_vec[0] = msg_to_Vec3d(msg.pose.position);
+    //R_vicon_vec[0] = msg_to_Quaterniond(msg.pose.orientation).toRotationMatrix();
+}
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "ee_mass_estimator");
     ros::NodeHandle n;
 
-    ros::Subscriber odar1_pose_sub = n.subscribe("", 1000, odar1_pose_callback);
+    ros::Subscriber link1_pose_sub = n.subscribe("", 1000, link1_pose_callback);
     ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
 
     ros::Rate loop_rate(10);
@@ -26,7 +43,7 @@ int main(int argc, char **argv)
     {
         std_msgs::String msg;
         std::stringstream ss;
-        ss << "test" << count;
+        ss << "test" << count <<g[2];
         msg.data = ss.str();
 
         ROS_INFO("%s", msg.data.c_str());
